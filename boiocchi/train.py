492f001adaf4df3413dfa515798306c5cc3d046d
Sapoboio <cristian.boiocchi@itsrizzoli.it>
2025-06-09 15:27:16 +0200

    Update train.py

--- boiocchi/train.py
+++ boiocchi/train.py
@@ -1,37 +1,34 @@
-import os
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split, RandomizedSearchCV, KFold
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, make_scorer, r2_score, mean_squared_error
+import os
 from joblib import dump
 
-# Carica dati e pulizia
+
+
 df = pd.read_csv('Salary_Data.csv')
+df[df.isnull().any(axis=1)]
 df.dropna(inplace=True)
-
-# Dividi train/test
+from sklearn.model_selection import train_test_split
 df_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
-
-# Definisci feature e target
+from sklearn.linear_model import LinearRegression
 X_train = df_train[["Years of Experience"]]
 y_train = df_train["Salary"]
 
 X_test = df_test[["Years of Experience"]]
 y_test = df_test["Salary"]
-
-# Allena modello
 linreg = LinearRegression()
 linreg.fit(X_train, y_train)
-
-# Imposta parametro positive=True se ti serve
+linreg.get_params()
 linreg.set_params(positive=True)
-
-# Previsione su test
 y_test_pred = linreg.predict(X_test)
+y_test_pred
 
-# Crea cartella se non esiste
 os.makedirs('boiocchi', exist_ok=True)
-
-# Salva modello con joblib
 dump(linreg, 'boiocchi/artifact.joblib')
-
-print("Modello salvato correttamente in 'boiocchi/artifact.joblib'")
+print("Modello salvato in 'boiocchi/artifact.joblib'")
+print("sddyt")
